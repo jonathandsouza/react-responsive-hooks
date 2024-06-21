b24d94ca6e20367abf1d864b323bb344020536cd
Jonathan Dsouza <mail2jona@yhoo.com>
2024-06-21 23:08:46 +0200

    initial implementation

--- src/viewport.tsx
+++ src/viewport.tsx
@@ -1,4 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+const DEFAULT_VIEWPORT_LIMITS = {
+	smallMobile: 320,
+	mobile: 480,
+	tablet: 768,
+	desktop: 1024,
+	largeDesktop: 1280,
+};
 
 type viewportLimits = {
 	smallMobile: number;
@@ -6,20 +14,49 @@ type viewportLimits = {
 	tablet: number;
 	desktop: number;
 	largeDesktop: number;
-} | null;
-
-type viewportValidity = {
-	[K in keyof viewportLimits]: boolean;
 };
 
-type props = {
+type viewportValidity =
+	| {
+			[K in keyof viewportLimits]: boolean;
+	  }
+	| null;
+
+type componentProps = {
 	children: React.ReactNode;
-} & viewportLimits;
+} & Omit<
+	viewportLimits,
+	'smallMobile' | 'mobile' | 'tablet' | 'desktop' | 'largeDesktop'
+>;
 
-const viewportContext = React.createContext<viewportLimits>(null);
+const viewportContext = React.createContext<viewportValidity>(null);
 
-const ViewportProvider = ({ children }: props) => {
-	const [width, setWidth] = React.useState<viewportValidity>({
+function useViewport() {
+	const context = React.useContext(viewportContext);
+	if (!context) {
+		throw new Error('useViewport must be used within a ViewportProvider');
+	}
+	return context;
+}
+
+function compileViewportValidity(
+	viewportLimits: viewportLimits = DEFAULT_VIEWPORT_LIMITS
+) {
+	const width = window.innerWidth;
+
+	const viewportValidity: viewportValidity = {
+		smallMobile: width >= viewportLimits.smallMobile,
+		mobile: width >= viewportLimits.mobile,
+		tablet: width >= viewportLimits.tablet,
+		desktop: width >= viewportLimits.desktop,
+		largeDesktop: width >= viewportLimits.largeDesktop,
+	};
+
+	return viewportValidity;
+}
+
+export const ViewportProvider = (...props: props){ => {
+	const [viewport, setViewport] = React.useState<viewportValidity>({
 		smallMobile: false,
 		mobile: false,
 		tablet: false,
@@ -27,15 +64,18 @@ const ViewportProvider = ({ children }: props) => {
 		largeDesktop: false,
 	});
 
+	const [viewportLimits, setViewportLimits] = useState(
+		Object.assign(props, DEFAULT_VIEWPORT_LIMITS)
+	);
+
 	useEffect(() => {
 		const handleWindowResize = () => {};
-
 		window.addEventListener('resize', handleWindowResize);
 		return () => window.removeEventListener('resize', handleWindowResize);
 	}, []);
 
 	return (
-		<viewportContext.Provider value={{ width, height }}>
+		<viewportContext.Provider value={viewport}>
 			{children}
 		</viewportContext.Provider>
 	);
