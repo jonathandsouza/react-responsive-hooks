3d1c8491f13f966893dc1f6231057acc2ab5f7b3
Jonathan Dsouza <mail2jona@yhoo.com>
2024-06-24 23:23:59 +0200

    added story book

--- app/demo/src/App.tsx
+++ app/demo/src/App.tsx
@@ -1,22 +1,30 @@
 import { useBreakpoint } from '@react-responsive/breakpoint';
 
 function App() {
-	const { smallMobile, mobile, tablet, desktop, largeDesktop } =
-		useBreakpoint();
+	const {
+		isDesktop,
+		isExtraLargeDesktop,
+		isLargeDesktop,
+		isMobile,
+		isSmallMobile,
+		isTablet,
+	} = useBreakpoint();
 
 	return (
 		<>
 			<h1>React responsive hooks: Breakpoints</h1>
 
-			{smallMobile && <h1>Small mobile</h1>}
+			{isSmallMobile && <h1>Small mobile</h1>}
 
-			{tablet && <h1>Tablet</h1>}
+			{isMobile && <h1>Mobile</h1>}
 
-			{desktop && <h1>Desktop</h1>}
+			{isTablet && <h1>Tablet</h1>}
 
-			{largeDesktop && <h1>Large desktop</h1>}
+			{isDesktop && <h1>Desktop</h1>}
 
-			{mobile && <h1>Mobile</h1>}
+			{isLargeDesktop && <h1>Large desktop</h1>}
+
+			{isExtraLargeDesktop && <h1>Large desktop</h1>}
 		</>
 	);
 }
